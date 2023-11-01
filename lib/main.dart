3a5005aec66e910bd2d146082f3ec33f985ee88d
mohammadaghigh <mohammadaghigh@gmail.com>
2023-11-01 03:38:05 +0330

    device orientation

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,13 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 
 import 'screens/home_screen.dart';
 
 void main() {
+  WidgetsFlutterBinding.ensureInitialized();
+  SystemChrome.setPreferredOrientations([
+    DeviceOrientation.portraitUp,
+  ]);
   runApp(Application());
 }
 
